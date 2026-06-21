2e151df5de3937a863a5444bb64ed995b235f088
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-21 23:17:26 +0100

    Lesson 1 - Variables and Input Output

--- variable/Program.cs
+++ variable/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = "Wisdom";
+        int age = 20;
+        Console.WriteLine($"Hello! {name}, who is {age} years");
+
+        Console.Write("Enter your name: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine($"Welcome {input}");
+    }
+}
