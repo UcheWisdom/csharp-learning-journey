2222f90df3eaf339853cff3291ddf1996558b952
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-22 01:13:07 +0100

    Lesson 2 - Conditions

---
+++ Lesson02-Conditions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (score >= 70)
+        {
+            Console.WriteLine("Grade A");
+        }
+        else if (score >= 50)
+        {
+            Console.WriteLine("Grade B");
+        }
+        else
+        {
+            Console.WriteLine("Grade F");
+        }
+    }
+}
