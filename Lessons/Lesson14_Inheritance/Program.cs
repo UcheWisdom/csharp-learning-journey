0732f2394e66ddd97cf237e8058534a6090151e2
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-07-03 15:11:23 +0100

    feat: create first inheritance hierarchy with person, student and teacher

--- Lessons/Lesson14_Inheritance/Program.cs
+++ Lessons/Lesson14_Inheritance/Program.cs
@@ -38,7 +38,20 @@ class Program
         };
 
 
-        Console.WriteLine($"Student: {student.Name}, Age: {student.Age}, Email: {student.Email}, Score: {student.Score}");
-        Console.WriteLine($"Teacher: {teacher.Name}, Age: {teacher.Age}, Email: {teacher.Email}, Salary: ${teacher.Salary}");
+        //Console.WriteLine($"Student: {student.Name}, Age: {student.Age}, Email: {student.Email}, Score: {student.Score}");
+        //Console.WriteLine($"Teacher: {teacher.Name}, Age: {teacher.Age}, Email: {teacher.Email}, Salary: ${teacher.Salary}");
+
+        Console.WriteLine("Student Information");
+        Console.WriteLine($"Name  : {student.Name}");
+        Console.WriteLine($"Age   : {student.Age}");
+        Console.WriteLine($"Email : {student.Email}");
+        Console.WriteLine($"Score : {student.Score}");
+        Console.WriteLine();
+        Console.WriteLine("Teacher Information");
+        Console.WriteLine($"Name  : {teacher.Name}");
+        Console.WriteLine($"Age   : {teacher.Age}");
+        Console.WriteLine($"Email : {teacher.Email}");
+        Console.WriteLine($"Salary : {teacher.Salary}");
+
     }
 }
